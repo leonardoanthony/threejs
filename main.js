c5542580f0f6fa3142ca7c169bce4c5679512b44
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-03-29 17:40:18 -0300

    :sparkles: feat: Primeiro exercício

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
 const w = window.innerWidth;
 const h = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -13,30 +12,46 @@ const aspect = w / h;
 const near = 0.1;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 50;
+camera.position.z = 3;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.TorusGeometry( 12, 1, 10, 6 ); 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
 
-const material = new THREE.MeshBasicMaterial( { color: 0xaf61fa } ); 
-const torus = new THREE.Mesh( geometry, material ); 
-scene.add( torus );
+const geometry = new THREE.IcosahedronGeometry(1, 3)
+const material = new THREE.MeshStandardMaterial({ 
+    color: 0xffffff,
+    flatShading: true, 
 
-const wireMaterial = new THREE.MeshBasicMaterial({
+}); 
+const mesh = new THREE.Mesh( geometry, material ); 
+scene.add( mesh );
+
+const wireMat = new THREE.MeshBasicMaterial({
     color: 0xffffff,
     wireframe: true
 })
 
-const wireMesh = new THREE.Mesh(geometry, wireMaterial);
+const wireMash = new THREE.Mesh(geometry, wireMat);
+// wireMash.scale.setScalar(1.001);
+mesh.add(wireMash);
 
-wireMesh.scale.setScalar(1.001);
-
-torus.add(wireMesh);
+const hemiLight = new THREE.HemisphereLight(0x9999ff, 0xaa5500);
+scene.add(hemiLight);
 
 function animate(){
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
+    mesh.rotation.y += 0.003
     renderer.render(scene, camera);
+    controls.update();
 }
 
 animate();
+
+// ver mais 
+
+// diferentes objetos primitivos
+// diferentes luzes ( direcional , solar, point and spotlight )
+// animação, posição, rotação, tamanho, cor do objeto e cor da luz
